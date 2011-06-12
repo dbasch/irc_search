a00f750698a10510d2974fadc4d7abe659c3333b
Diego Basch <dbasch@gmail.com>
2011-06-12 14:42:10 -0700

    made more robust against exceptions

--- indexer.rb
+++ indexer.rb
@@ -15,8 +15,8 @@ index = api.indexes idxname
 if not index.exists?
   index.add 
   while not index.running?
-      sleep 0.5
-      printf "waiting for index %s to be ready...\n", idxname
+    sleep 0.5
+    printf "waiting for index %s to be ready...\n", idxname
   end
 end
 printf "Ready.\n"
@@ -31,39 +31,45 @@ if res['matches'] != 0
 end
 
 printf "next docid is %d\n", docid
-   
+
 
 while true
-	s = TCPSocket.open(config['server'], config['port'])
-	print("addr: ", s.addr.join(":"), "\n")
-	print("peer: ", s.peeraddr.join(":"), "\n")
-	s.puts "USER " + nick + " 0 * " + config['name']
-	s.puts "NICK #{nick}"
-	s.puts "JOIN #{chan}"
+  s = TCPSocket.open(config['server'], config['port'])
+  print("addr: ", s.addr.join(":"), "\n")
+  print("peer: ", s.peeraddr.join(":"), "\n")
+  s.puts "USER " + nick + " 0 * " + config['name']
+  s.puts "NICK #{nick}"
+  s.puts "JOIN #{chan}"
 
-	until s.eof? do
-		msg = s.gets
-		if msg.match /^PING/
-		    s.puts msg.gsub 'PING', 'PONG'
-		else
-    puts msg
-        parts = msg.split DELIM
-        if parts.size > 1
-          user = parts[0].split('!')[0][1..-1]
-          rest = parts[1..-1].join DELIM
-          text = rest[2 + chan.size..-1]
-          printf "docid: %d, user: %s, text %s", docid, user, text
-          ts = Time.now.to_i
-          if init
-            index.document("nextid").add({:nextid => :nextid})
-            init = false
-          end
-          index.document(docid.to_s).add({ :user => user, :text => user + ' ' + text, :timestamp => ts, :matchall => :all}, :variables => {1 => docid})
-          docid += 1
-          index.document(:nextid).update_variables(0 => docid)
+  until s.eof? do
+    msg = s.gets
+    if msg.match /^PING/
+      s.puts msg.gsub 'PING', 'PONG'
+    else
+      puts msg
+      parts = msg.split DELIM
+      if parts.size > 1
+        user = parts[0].split('!')[0][1..-1]
+        rest = parts[1..-1].join DELIM
+        text = rest[2 + chan.size..-1]
+        printf "docid: %d, user: %s, text %s", docid, user, text
+        ts = Time.now.to_i
+        if init
+          index.document("nextid").add({:nextid => :nextid})
+          init = false
         end
-		    STDOUT.flush
-		end
-	end
- sleep 2
+
+        begin
+          index.document(docid.to_s).add({ :user => user, :text => user + ' ' + text, :timestamp => ts, :matchall => :all}, :variables => {1 => docid})
+          index.document(:nextid).update_variables(0 => docid+1)
+        rescue
+          sleep 1
+          retry
+        end
+        docid += 1
+      end
+      STDOUT.flush
+    end
+  end
+  sleep 2
 end
